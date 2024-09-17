179dee949f3df7b21326a5393d78ec83fe1df5b2
Mohammed Bilal N S <70318383+mohammedbilalns@users.noreply.github.com>
2024-09-17 13:13:48 +0530

    implement: sidebar collapse function

--- scripts/script.js
+++ scripts/script.js
@@ -9,3 +9,20 @@ function toggleDropdown(){
     }
 }
 
+
+function toggleSidebar(){
+    const sidebar = document.querySelector(".sidebar");
+    const topButtonsContainer = document.querySelector(".top-buttons-container");
+    const topButtonsLeftSection = document.querySelector(".top-buttons-left-section");
+    if(sidebar.style.display === "flex"){
+        sidebar.style.display = "none";
+        topButtonsContainer.style.backgroundColor = "white";
+        topButtonsContainer.style.width = "80px";
+        topButtonsLeftSection.style.marginLeft = "80px";
+    }else{
+        sidebar.style.display = "flex";
+        topButtonsContainer.style.backgroundColor = "#F9F9F9";
+        topButtonsContainer.style.width = "240px";
+        topButtonsLeftSection.style.marginLeft = "0px";
+    }
+}
